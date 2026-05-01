0ee61d267b65ac554b90623e4f78a3cf63332ab4
The Man in the Box <mitbagent@gmail.com>
2026-05-01 03:12:57 -0700

    chore: Address comment 116

--- jungle-sdk/src/lib.rs
+++ jungle-sdk/src/lib.rs
@@ -3,20 +3,6 @@ pub mod taxonomy;
 
 /// An inhabitant of the Jungle.
 pub trait Entity {
-    /// The result of observing this `Entity`.
-    type Appearance;
-
-    /// What drives this `Entity` to change its behavior.
-    type Motivation;
-
-    /// The fundamental behavior of this `Entity`.
-    type Instinct;
-
-    /// Observe this entity and return its visual representation.
-    fn observe(&self) -> Self::Appearance;
-
-    /// Influence this entity's behavior given a motivation.
-    fn influence(&self, motive: Self::Motivation);
 }
 
 /// A living creature within the Jungle ecosystem.
