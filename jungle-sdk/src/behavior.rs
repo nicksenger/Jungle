cc82524f312c03bf20ad2ff65f509e10787ed3f1
The Man in the Box <mitbagent@gmail.com>
2026-05-01 02:18:41 -0700

    chore: Address comment 73

---
+++ jungle-sdk/src/behavior.rs
@@ -0,0 +1,17 @@
+/// Behavior traits for Jungle entities.
+///
+/// These traits define the contract for actions an entity can take
+/// and the impulses that drive those actions.
+
+use crate::Niche;
+
+/// A trait for types that define the input/output contract of an action.
+pub trait Impulse {
+    type Input: serde::Serialize + serde::de::DeserializeOwned;
+    type Output: serde::Serialize + serde::de::DeserializeOwned;
+}
+
+/// A trait for actions, which are a kind of impulse scoped to a niche.
+pub trait Action: Impulse {
+    type Niche: Niche;
+}
