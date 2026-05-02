e40cc282a1458ff7966a8d0d901ad7792207c38c
The Man in the Box <mitbagent@gmail.com>
2026-05-02 00:49:26 -0700

    chore: Address comment 185

--- jungle-types/src/behavior.rs
+++ jungle-types/src/behavior.rs
@@ -3,16 +3,12 @@
 /// These traits define the contract for actions an entity can take
 /// and the impulses that drive those actions.
 
-/// Niche for an ecological role
-pub trait Niche {}
-
 /// A trait for types that define the input/output contract of an action.
 pub trait Impulse {
     type Input: serde::Serialize + serde::de::DeserializeOwned;
     type Output: serde::Serialize + serde::de::DeserializeOwned;
 }
 
-/// A trait for actions, which are a kind of impulse scoped to a niche.
+/// A trait for actions, which are a kind of impulse.
 pub trait Action: Impulse {
-    type Niche: Niche;
 }
