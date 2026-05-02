f59522f7cada8ffe7255021c62a1688f9ba81e58
The Man in the Box <mitbagent@gmail.com>
2026-05-02 01:49:44 -0700

    chore: Address comment 239

--- jungle-types/src/taxonomy.rs
+++ jungle-types/src/taxonomy.rs
@@ -6,29 +6,19 @@ use crate::Animal;
 /// species and animals within the ecosystem.
 
 /// Phylum-level grouping
-pub trait Phylum {
-    type Taxa;
-}
+pub trait Phylum {}
 
 /// Class-level grouping
-pub trait Class {
-    type Taxa;
-}
+pub trait Class {}
 
 /// Order-level grouping
-pub trait Order {
-    type Taxa;
-}
+pub trait Order {}
 
 /// Family-level grouping
-pub trait Family {
-    type Taxa;
-}
+pub trait Family {}
 
 /// Grouping for `Species`
-pub trait Genus {
-    type Taxa;
-}
+pub trait Genus {}
 
 /// Grouping for `Animal`
 pub trait Species {}
@@ -53,11 +43,6 @@ impl<T: Leaf> Flattened for T {
     type Out = typosaurus::collections::list::List<(T, typosaurus::collections::list::Empty)>;
 }
 
-/// Genus-level types flatten by recursing into their `Taxa`.
-impl<G: Genus> Flattened for G {
-    type Out = <G::Taxa as Flattened>::Out;
-}
-
 /// A list of flattened elements is the append of each element's flatten.
 impl<H, T> Flattened for typosaurus::collections::list::List<(H, T)>
 where
@@ -73,71 +58,3 @@ pub type Flatten<T> = <T as typosaurus::traits::fold::Foldable<Concatenation>>::
 
 /// Semigroup for concatenating flattened taxonomy types.
 pub struct Concatenation;
-
-impl<Lhs, Rhs> typosaurus::traits::monoid::Mappend for (Lhs, Rhs)
-where
-    Lhs: Flattened,
-    Rhs: Flattened,
-    (<Lhs as Flattened>::Out, <Rhs as Flattened>::Out): typosaurus::traits::monoid::Mappend,
-{
-    type Mappend = <(<Lhs as Flattened>::Out, <Rhs as Flattened>::Out) as typosaurus::traits::monoid::Mappend>::Out;
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use typosaurus::collections::list::Flattened as TyFlattened;
-    use typosaurus::assert_type_eq;
-
-    // --- Example from the PR: dog, cat, wolf ---
-
-    struct Dog;
-    impl Animal for Dog {
-        type Form = ();
-        type Motivation = ();
-        type Instinct = ();
-    }
-    impl Leaf for Dog {}
-
-    struct Wolf;
-    impl Animal for Wolf {
-        type Form = ();
-        type Motivation = ();
-        type Instinct = ();
-    }
-    impl Leaf for Wolf {}
-
-    struct Cat;
-    impl Animal for Cat {
-        type Form = ();
-        type Motivation = ();
-        type Instinct = ();
-    }
-    impl Leaf for Cat {}
-
-    struct Canis;
-    impl Genus for Canis {
-        type Taxa = list![Dog, Wolf];
-    }
-
-    #[test]
-    fn test_canis_flattens_to_dog_and_wolf() {
-        type Flat = <Canis as TyFlattened>::Out;
-        assert_type_eq!(Flat, list![Dog, Wolf]);
-    }
-
-    #[test]
-    fn test_list_canis_cat_flattens() {
-        // list![Canis, Cat] → list![Dog, Wolf, Cat]
-        type Input = list![Canis, Cat];
-        type Result = <Input as TyFlattened>::Out;
-        assert_type_eq!(Result, list![Dog, Wolf, Cat]);
-    }
-
-    #[test]
-    fn test_flatten_alias() {
-        type Input = list![Canis, Cat];
-        type Result = Flatten<Input>;
-        assert_type_eq!(Result, list![Dog, Wolf, Cat]);
-    }
-}
