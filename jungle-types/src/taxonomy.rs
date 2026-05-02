68e6361814b14f81f82db0f21ec23caac5e24244
The Man in the Box <mitbagent@gmail.com>
2026-05-02 01:55:46 -0700

    chore: Address comment 245

--- jungle-types/src/taxonomy.rs
+++ jungle-types/src/taxonomy.rs
@@ -29,32 +29,3 @@ where
 {
 }
 
-/// Marker for species-level animals that are atomic leaf types.
-/// Types implementing `Leaf` flatten to a singleton list.
-pub trait Leaf {}
-
-/// `Empty` (empty list) flattens to `Empty`.
-impl Flattened for typosaurus::collections::list::Empty {
-    type Out = typosaurus::collections::list::Empty;
-}
-
-/// Leaf (species-level) types flatten to a singleton list.
-impl<T: Leaf> Flattened for T {
-    type Out = typosaurus::collections::list::List<(T, typosaurus::collections::list::Empty)>;
-}
-
-/// A list of flattened elements is the append of each element's flatten.
-impl<H, T> Flattened for typosaurus::collections::list::List<(H, T)>
-where
-    H: Flattened,
-    T: Flattened,
-    (<H as Flattened>::Out, <T as Flattened>::Out): typosaurus::traits::monoid::Mappend,
-{
-    type Out = <(<H as Flattened>::Out, <T as Flattened>::Out) as typosaurus::traits::monoid::Mappend>::Out;
-}
-
-/// Convenience type alias to flatten any taxonomy entity into a flat `list!` of animals.
-pub type Flatten<T> = <T as typosaurus::traits::fold::Foldable<Concatenation>>::Out;
-
-/// Semigroup for concatenating flattened taxonomy types.
-pub struct Concatenation;
