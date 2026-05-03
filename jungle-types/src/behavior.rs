89ef650c54e23343b50b39ba6986b79df4561a71
The Man in the Box <mitbagent@gmail.com>
2026-05-03 02:45:13 -0700

    chore: Address comment 483

--- jungle-types/src/behavior.rs
+++ jungle-types/src/behavior.rs
@@ -1,6 +1,12 @@
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 
+/// The innate behavior definition for an Animal.
+pub trait Instinct {
+    /// The actions available to this instinct.
+    type Actions;
+}
+
 /// A behavior that transforms a single input into a single output.
 pub trait Action {
     /// The input type accepted by this action.
--- jungle-types/src/lib.rs
+++ jungle-types/src/lib.rs
@@ -1,6 +1,6 @@
 mod behavior;
 mod meta;
-pub use behavior::Action;
+pub use behavior::{Action, Instinct};
 pub use meta::Id;
 
 /// A living creature within the Jungle ecosystem.
