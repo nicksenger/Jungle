0588273612845dac97f496561c550d787845b8b0
The Man in the Box <mitbagent@gmail.com>
2026-05-03 00:56:40 -0700

    chore: Address comment 402

--- jungle-core/src/lib.rs
+++ jungle-core/src/lib.rs
@@ -1,19 +1,7 @@
 /// An inhabitant of the Jungle.
 pub trait Entity {
-    /// The result of observing this `Entity`.
-    type Appearance;
-
-    /// What drives this `Entity` to change its behavior.
-    type Motivation;
-
     /// The fundamental behavior of this `Entity`.
     type Instinct;
-
-    /// Observe this entity and return its visual representation.
-    fn observe(&self) -> Self::Appearance;
-
-    /// Influence this entity's behavior given a motivation.
-    fn influence(&self, motive: Self::Motivation);
 }
 
 pub trait Jungle {}
