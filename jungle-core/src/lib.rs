e003c57c31e126e80c96b30eca797590b2b76bf5
The Man in the Box <mitbagent@gmail.com>
2026-05-03 02:57:16 -0700

    chore: Address comment 492

--- jungle-core/src/lib.rs
+++ jungle-core/src/lib.rs
@@ -1,11 +1,4 @@
-/// An inhabitant of the Jungle.
-pub trait Entity {
-    /// The fundamental behavior of this `Entity`.
-    type Instinct;
-}
-
 pub trait Jungle {}
 
 /// A worker carries out the will of the Jungle.
 pub trait Worker<T: Jungle> {}
-
