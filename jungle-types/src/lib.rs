a2485de54c7e8c849f48206d83f30e6b49ffe177
The Man in the Box <mitbagent@gmail.com>
2026-05-04 01:09:56 -0700

    chore: Address comment 580

--- jungle-types/src/lib.rs
+++ jungle-types/src/lib.rs
@@ -7,8 +7,6 @@ pub use behavior::{Action, Instinct};
 pub use error::Error;
 pub use meta::Id;
 use inception::*;
-use typosaurus::collections::list;
-use typosaurus::traits::semigroup::Mappend;
 
 /// A collection of Jungle entities and the Animals that fill them.
 ///
@@ -32,9 +30,9 @@ pub trait Animal {
 #[inception(property = JungleAnimal, types)]
 pub trait Animals {
     #[induce(
-        base = list::Empty,
-        merge = <(<Head as Animals>::List, <Tail as Animals>::List) as Mappend>::Out where { (<Head as Animals>::List, <Tail as Animals>::List): Mappend },
-        merge_variant = <(<Head as Animals>::List, <Tail as Animals>::List) as Mappend>::Out where { (<Head as Animals>::List, <Tail as Animals>::List): Mappend },
+        base = meta::EmptyList,
+        merge = meta::Merge<<Head as Animals>::List, <Tail as Animals>::List> where { (<Head as Animals>::List, <Tail as Animals>::List): meta::ListMappend },
+        merge_variant = meta::Merge<<Head as Animals>::List, <Tail as Animals>::List> where { (<Head as Animals>::List, <Tail as Animals>::List): meta::ListMappend },
         join = <Fields as Animals>::List
     )]
     type List;
@@ -44,9 +42,9 @@ pub trait Animals {
 #[inception(property = JungleAction, types)]
 pub trait Actions {
     #[induce(
-        base = list::Empty,
-        merge = <(<Head as Actions>::List, <Tail as Actions>::List) as Mappend>::Out where { (<Head as Actions>::List, <Tail as Actions>::List): Mappend },
-        merge_variant = <(<Head as Actions>::List, <Tail as Actions>::List) as Mappend>::Out where { (<Head as Actions>::List, <Tail as Actions>::List): Mappend },
+        base = meta::EmptyList,
+        merge = meta::Merge<<Head as Actions>::List, <Tail as Actions>::List> where { (<Head as Actions>::List, <Tail as Actions>::List): meta::ListMappend },
+        merge_variant = meta::Merge<<Head as Actions>::List, <Tail as Actions>::List> where { (<Head as Actions>::List, <Tail as Actions>::List): meta::ListMappend },
         join = <Fields as Actions>::List
     )]
     type List;
--- jungle-types/src/meta/mod.rs
+++ jungle-types/src/meta/mod.rs
@@ -4,6 +4,15 @@ use typosaurus::num::Unsigned;
 /// Newtype wrapper around an Unsigned constant.
 pub struct Id<T: Unsigned>(pub T);
 
+/// Type-level empty list used by collection metadata in this crate.
+pub type EmptyList = typosaurus::collections::list::Empty;
+
+/// Type-level append trait for metadata list composition.
+pub use typosaurus::traits::semigroup::Mappend as ListMappend;
+
+/// Type-level append output of two metadata lists.
+pub type Merge<Lhs, Rhs> = <(Lhs, Rhs) as ListMappend>::Out;
+
 /// Blanket impl: `Id<T>` is equal to `Id<U>` iff `T` is equal to `U`.
 impl<T, U> Equality<Id<U>> for Id<T>
 where
