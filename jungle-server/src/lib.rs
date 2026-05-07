88d98ee227ae6473382a86313882e79ab8dd7ed9
The Man in the Box <mitbagent@gmail.com>
2026-05-07 03:50:20 -0700

    chore: Address comment 1452

--- jungle-server/src/lib.rs
+++ jungle-server/src/lib.rs
@@ -1,6 +1,5 @@
 use std::{fs, io, net::SocketAddr, path::PathBuf, sync::Arc};
 
-use clap::Parser;
 use quinn::crypto::rustls::QuicServerConfig;
 use rustls::pki_types::{pem::PemObject, CertificateDer, PrivateKeyDer, PrivatePkcs8KeyDer};
 use thiserror::Error;
@@ -8,35 +7,192 @@ use tracing::{error, info, info_span};
 use tracing_futures::Instrument as _;
 
 const ALPN_QUIC_HTTP: &[&[u8]] = &[b"hq-29"];
+const DEFAULT_LISTEN_ADDR: &str = "[::1]:4433";
 
-#[derive(Parser, Debug, Clone)]
-#[clap(name = "jungle-server")]
-pub struct Opt {
-    /// file to log TLS keys to for debugging
-    #[clap(long = "keylog")]
-    pub keylog: bool,
-    /// TLS private key in PEM or DER format
-    #[clap(short = 'k', long = "key", requires = "cert")]
-    pub key: Option<PathBuf>,
-    /// TLS certificate in PEM or DER format
-    #[clap(short = 'c', long = "cert", requires = "key")]
-    pub cert: Option<PathBuf>,
-    /// Enable stateless retries
-    #[clap(long = "stateless-retry")]
-    pub stateless_retry: bool,
-    /// Address to listen on
-    #[clap(long = "listen", default_value = "[::1]:4433")]
-    pub listen: SocketAddr,
-    /// Client address to block
-    #[clap(long = "block")]
-    pub block: Option<SocketAddr>,
-    /// Maximum number of concurrent connections to allow
-    #[clap(long = "connection-limit")]
-    pub connection_limit: Option<usize>,
+#[derive(Debug, Clone)]
+pub struct ServerBuilder {
+    keylog: bool,
+    key: Option<PathBuf>,
+    cert: Option<PathBuf>,
+    stateless_retry: bool,
+    listen: SocketAddr,
+    block: Option<SocketAddr>,
+    connection_limit: Option<usize>,
+}
+
+impl Default for ServerBuilder {
+    fn default() -> Self {
+        Self {
+            keylog: false,
+            key: None,
+            cert: None,
+            stateless_retry: false,
+            listen: DEFAULT_LISTEN_ADDR
+                .parse()
+                .expect("default listen address must be valid"),
+            block: None,
+            connection_limit: None,
+        }
+    }
+}
+
+impl ServerBuilder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn keylog(mut self, enabled: bool) -> Self {
+        self.keylog = enabled;
+        self
+    }
+
+    pub fn key(mut self, key: impl Into<PathBuf>) -> Self {
+        self.key = Some(key.into());
+        self
+    }
+
+    pub fn cert(mut self, cert: impl Into<PathBuf>) -> Self {
+        self.cert = Some(cert.into());
+        self
+    }
+
+    pub fn stateless_retry(mut self, enabled: bool) -> Self {
+        self.stateless_retry = enabled;
+        self
+    }
+
+    pub fn listen(mut self, addr: SocketAddr) -> Self {
+        self.listen = addr;
+        self
+    }
+
+    pub fn block(mut self, addr: SocketAddr) -> Self {
+        self.block = Some(addr);
+        self
+    }
+
+    pub fn connection_limit(mut self, limit: usize) -> Self {
+        self.connection_limit = Some(limit);
+        self
+    }
+
+    pub async fn run(self) -> Result<()> {
+        let (certs, key) = match (&self.key, &self.cert) {
+            (Some(key_path), Some(cert_path)) => load_user_cert_chain_and_key(key_path, cert_path)?,
+            (None, None) => load_or_generate_self_signed_cert()?,
+            _ => return Err(ServerError::MissingKeyOrCertPair),
+        };
+
+        let mut server_crypto = rustls::ServerConfig::builder()
+            .with_no_client_auth()
+            .with_single_cert(certs, key)
+            .map_err(ServerError::RustlsCertConfig)?;
+        server_crypto.alpn_protocols = ALPN_QUIC_HTTP.iter().map(|&x| x.into()).collect();
+        if self.keylog {
+            server_crypto.key_log = Arc::new(rustls::KeyLogFile::new());
+        }
+
+        let mut server_config = quinn::ServerConfig::with_crypto(Arc::new(
+            QuicServerConfig::try_from(server_crypto).map_err(ServerError::QuicServerConfig)?,
+        ));
+        let transport_config = Arc::get_mut(&mut server_config.transport).unwrap();
+        transport_config.max_concurrent_uni_streams(0_u8.into());
+
+        let endpoint = quinn::Endpoint::server(server_config, self.listen)
+            .map_err(ServerError::BindEndpoint)?;
+        eprintln!(
+            "listening on {}",
+            endpoint.local_addr().map_err(ServerError::LocalAddr)?
+        );
+
+        while let Some(conn) = endpoint.accept().await {
+            if self
+                .connection_limit
+                .is_some_and(|n| endpoint.open_connections() >= n)
+            {
+                info!("refusing due to open connection limit");
+                conn.refuse();
+            } else if Some(conn.remote_address()) == self.block {
+                info!("refusing blocked client IP address");
+                conn.refuse();
+            } else if self.stateless_retry && !conn.remote_address_validated() {
+                info!("requiring connection to validate its address");
+                let _ = conn.retry();
+            } else {
+                info!("accepting connection");
+                let fut = handle_connection(conn);
+                tokio::spawn(async move {
+                    if let Err(e) = fut.await {
+                        error!("connection failed: {reason}", reason = e.to_string())
+                    }
+                });
+            }
+        }
+
+        Ok(())
+    }
+}
+
+fn load_user_cert_chain_and_key(
+    key_path: &PathBuf,
+    cert_path: &PathBuf,
+) -> Result<(Vec<CertificateDer<'static>>, PrivateKeyDer<'static>)> {
+    let key = if key_path.extension().is_some_and(|x| x == "der") {
+        PrivateKeyDer::Pkcs8(PrivatePkcs8KeyDer::from(
+            fs::read(key_path).map_err(ServerError::ReadPrivateKeyFile)?,
+        ))
+    } else {
+        PrivateKeyDer::from_pem_file(key_path).map_err(ServerError::ReadPrivateKeyPem)?
+    };
+
+    let cert_chain = if cert_path.extension().is_some_and(|x| x == "der") {
+        vec![CertificateDer::from(
+            fs::read(cert_path).map_err(ServerError::ReadCertChainFile)?,
+        )]
+    } else {
+        CertificateDer::pem_file_iter(cert_path)
+            .map_err(ServerError::ReadCertChainPem)?
+            .collect::<std::result::Result<Vec<_>, _>>()
+            .map_err(ServerError::InvalidPemCert)?
+    };
+
+    Ok((cert_chain, key))
+}
+
+fn load_or_generate_self_signed_cert(
+) -> Result<(Vec<CertificateDer<'static>>, PrivateKeyDer<'static>)> {
+    let dirs = directories_next::ProjectDirs::from("org", "jungle", "jungle-server").unwrap();
+    let path = dirs.data_local_dir();
+    let cert_path = path.join("cert.der");
+    let key_path = path.join("key.der");
+
+    let (cert, key) = match fs::read(&cert_path).and_then(|x| Ok((x, fs::read(&key_path)?))) {
+        Ok((cert, key)) => (
+            CertificateDer::from(cert),
+            PrivateKeyDer::try_from(key).map_err(|e| ServerError::ParseDerKey(e.to_owned()))?,
+        ),
+        Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
+            info!("generating self-signed certificate");
+            let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
+            let key = PrivatePkcs8KeyDer::from(cert.signing_key.serialize_der());
+            let cert = cert.cert.into();
+            fs::create_dir_all(path).map_err(ServerError::CreateCertDir)?;
+            fs::write(&cert_path, &cert).map_err(ServerError::WriteCert)?;
+            fs::write(&key_path, key.secret_pkcs8_der()).map_err(ServerError::WritePrivateKey)?;
+            (cert, key.into())
+        }
+        Err(e) => {
+            return Err(ServerError::ReadCertificate(e));
+        }
+    };
+
+    Ok((vec![cert], key))
 }
 
 #[derive(Debug, Error)]
 pub enum ServerError {
+    #[error("private key and certificate must either both be set or both omitted")]
+    MissingKeyOrCertPair,
     #[error("failed to read private key file: {0}")]
     ReadPrivateKeyFile(#[source] io::Error),
     #[error("failed to read PEM from private key file: {0}")]
@@ -83,106 +239,6 @@ pub fn init_tracing() -> std::result::Result<(), tracing::subscriber::SetGlobalD
             .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
             .finish(),
     )
-}
-
-pub async fn run(options: Opt) -> Result<()> {
-    let (certs, key) = if let (Some(key_path), Some(cert_path)) = (&options.key, &options.cert) {
-        let key = if key_path.extension().is_some_and(|x| x == "der") {
-            PrivateKeyDer::Pkcs8(PrivatePkcs8KeyDer::from(
-                fs::read(key_path).map_err(ServerError::ReadPrivateKeyFile)?,
-            ))
-        } else {
-            PrivateKeyDer::from_pem_file(key_path).map_err(ServerError::ReadPrivateKeyPem)?
-        };
-
-        let cert_chain = if cert_path.extension().is_some_and(|x| x == "der") {
-            vec![CertificateDer::from(
-                fs::read(cert_path).map_err(ServerError::ReadCertChainFile)?,
-            )]
-        } else {
-            CertificateDer::pem_file_iter(cert_path)
-                .map_err(ServerError::ReadCertChainPem)?
-                .collect::<std::result::Result<Vec<_>, _>>()
-                .map_err(ServerError::InvalidPemCert)?
-        };
-
-        (cert_chain, key)
-    } else {
-        let dirs = directories_next::ProjectDirs::from("org", "jungle", "jungle-server").unwrap();
-        let path = dirs.data_local_dir();
-        let cert_path = path.join("cert.der");
-        let key_path = path.join("key.der");
-        let (cert, key) = match fs::read(&cert_path).and_then(|x| Ok((x, fs::read(&key_path)?))) {
-            Ok((cert, key)) => (
-                CertificateDer::from(cert),
-                PrivateKeyDer::try_from(key).map_err(|e| ServerError::ParseDerKey(e.to_owned()))?,
-            ),
-            Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
-                info!("generating self-signed certificate");
-                let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
-                let key = PrivatePkcs8KeyDer::from(cert.signing_key.serialize_der());
-                let cert = cert.cert.into();
-                fs::create_dir_all(path).map_err(ServerError::CreateCertDir)?;
-                fs::write(&cert_path, &cert).map_err(ServerError::WriteCert)?;
-                fs::write(&key_path, key.secret_pkcs8_der())
-                    .map_err(ServerError::WritePrivateKey)?;
-                (cert, key.into())
-            }
-            Err(e) => {
-                return Err(ServerError::ReadCertificate(e));
-            }
-        };
-
-        (vec![cert], key)
-    };
-
-    let mut server_crypto = rustls::ServerConfig::builder()
-        .with_no_client_auth()
-        .with_single_cert(certs, key)
-        .map_err(ServerError::RustlsCertConfig)?;
-    server_crypto.alpn_protocols = ALPN_QUIC_HTTP.iter().map(|&x| x.into()).collect();
-    if options.keylog {
-        server_crypto.key_log = Arc::new(rustls::KeyLogFile::new());
-    }
-
-    let mut server_config = quinn::ServerConfig::with_crypto(Arc::new(
-        QuicServerConfig::try_from(server_crypto).map_err(ServerError::QuicServerConfig)?,
-    ));
-    let transport_config = Arc::get_mut(&mut server_config.transport).unwrap();
-    transport_config.max_concurrent_uni_streams(0_u8.into());
-
-    let endpoint = quinn::Endpoint::server(server_config, options.listen)
-        .map_err(ServerError::BindEndpoint)?;
-    eprintln!(
-        "listening on {}",
-        endpoint.local_addr().map_err(ServerError::LocalAddr)?
-    );
-
-    while let Some(conn) = endpoint.accept().await {
-        if options
-            .connection_limit
-            .is_some_and(|n| endpoint.open_connections() >= n)
-        {
-            info!("refusing due to open connection limit");
-            conn.refuse();
-        } else if Some(conn.remote_address()) == options.block {
-            info!("refusing blocked client IP address");
-            conn.refuse();
-        } else if options.stateless_retry && !conn.remote_address_validated() {
-            info!("requiring connection to validate its address");
-            let _ = conn.retry();
-        } else {
-            info!("accepting connection");
-            let fut = handle_connection(conn);
-            tokio::spawn(async move {
-                if let Err(e) = fut.await {
-                    error!("connection failed: {reason}", reason = e.to_string())
-                }
-            });
-        }
-    }
-
-    Ok(())
 }
 
 async fn handle_connection(conn: quinn::Incoming) -> Result<()> {
--- jungle-server/src/main.rs
+++ jungle-server/src/main.rs
@@ -1,4 +1,56 @@
+use std::{net::SocketAddr, path::PathBuf};
+
 use clap::Parser;
+
+#[derive(Parser, Debug, Clone)]
+#[clap(name = "jungle-server")]
+struct Opt {
+    /// file to log TLS keys to for debugging
+    #[clap(long = "keylog")]
+    keylog: bool,
+    /// TLS private key in PEM or DER format
+    #[clap(short = 'k', long = "key", requires = "cert")]
+    key: Option<PathBuf>,
+    /// TLS certificate in PEM or DER format
+    #[clap(short = 'c', long = "cert", requires = "key")]
+    cert: Option<PathBuf>,
+    /// Enable stateless retries
+    #[clap(long = "stateless-retry")]
+    stateless_retry: bool,
+    /// Address to listen on
+    #[clap(long = "listen", default_value = "[::1]:4433")]
+    listen: SocketAddr,
+    /// Client address to block
+    #[clap(long = "block")]
+    block: Option<SocketAddr>,
+    /// Maximum number of concurrent connections to allow
+    #[clap(long = "connection-limit")]
+    connection_limit: Option<usize>,
+}
+
+impl From<Opt> for jungle_server::ServerBuilder {
+    fn from(opt: Opt) -> Self {
+        let mut builder = jungle_server::ServerBuilder::new()
+            .keylog(opt.keylog)
+            .stateless_retry(opt.stateless_retry)
+            .listen(opt.listen);
+
+        if let Some(key) = opt.key {
+            builder = builder.key(key);
+        }
+        if let Some(cert) = opt.cert {
+            builder = builder.cert(cert);
+        }
+        if let Some(block) = opt.block {
+            builder = builder.block(block);
+        }
+        if let Some(connection_limit) = opt.connection_limit {
+            builder = builder.connection_limit(connection_limit);
+        }
+
+        builder
+    }
+}
 
 fn main() {
     if jungle_server::init_tracing().is_err() {
@@ -6,9 +58,9 @@ fn main() {
         std::process::exit(1);
     }
 
-    let opt = jungle_server::Opt::parse();
+    let builder = jungle_server::ServerBuilder::from(Opt::parse());
     let rt = tokio::runtime::Runtime::new().expect("failed to create tokio runtime");
-    let code = if let Err(e) = rt.block_on(jungle_server::run(opt)) {
+    let code = if let Err(e) = rt.block_on(builder.run()) {
         eprintln!("ERROR: {e}");
         1
     } else {
