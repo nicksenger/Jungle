ad0aa36a5d2cddfa1547b482756434009dfbf781
The Man in the Box <mitbagent@gmail.com>
2026-05-07 03:26:13 -0700

    chore: Remove unused

--- jungle-types/src/meta/mod.rs
+++ jungle-types/src/meta/mod.rs
@@ -12,15 +12,6 @@ use super::{Action, Actions, Creature, Creatures, FlowActions, Instinct};
 /// Newtype wrapper around an Unsigned constant.
 pub struct Id<T: Unsigned>(pub T);
 
-/// Type-level empty list used by collection metadata in this crate.
-pub type EmptyList = typosaurus::collections::list::Empty;
-
-/// Type-level append trait for metadata list composition.
-pub use typosaurus::traits::semigroup::Mappend as ListMappend;
-
-/// Type-level append output of two metadata lists.
-pub type Merge<Lhs, Rhs> = <(Lhs, Rhs) as ListMappend>::Out;
-
 /// Blanket impl: `Id<T>` is equal to `Id<U>` iff `T` is equal to `U`.
 impl<T, U> Equality<Id<U>> for Id<T>
 where
@@ -129,10 +120,11 @@ where
 pub type CreatureActionMembers<T> =
     <SPFlatten<<CreatureSet<T> as CollectCreatureInstinctActions>::Out> as StripActionHeaders>::Out;
 
-pub type CreatureActionDependencies<T> = <(CreatureActionMembers<T>, WithActionDependency) as Map<
-    <CreatureActionMembers<T> as Container>::Content,
-    WithActionDependency,
->>::Out;
+pub type CreatureActionDependencies<T> =
+    <(CreatureActionMembers<T>, WithActionDependency) as Map<
+        <CreatureActionMembers<T> as Container>::Content,
+        WithActionDependency,
+    >>::Out;
 
 pub trait CreatureStatesCompatible<From>: Creatures {}
 impl<T, From> CreatureStatesCompatible<From> for T
