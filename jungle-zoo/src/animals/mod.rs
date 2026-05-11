09399c7600ec2d51af971faf725af9db8741692d
The Man in the Box <mitbagent@gmail.com>
2026-05-11 02:48:49 -0700

    chore: Address comment 2280

---
+++ jungle-zoo/src/animals/mod.rs
@@ -0,0 +1,6 @@
+pub mod crocodile;
+pub mod giraffe;
+pub mod gorilla;
+pub mod hippo;
+pub mod lion;
+pub mod rhino;
--- jungle-zoo/src/lib.rs
+++ jungle-zoo/src/lib.rs
@@ -1,8 +1,3 @@
 //! Stub library crate for workspace membership.
 
-pub mod crocodile;
-pub mod giraffe;
-pub mod gorilla;
-pub mod hippo;
-pub mod lion;
-pub mod rhino;
+pub mod animals;
