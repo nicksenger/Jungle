bf562a69265ee3c3c000ef3032ac8bd06ccd18d9
The Man in the Box <mitbagent@gmail.com>
2026-05-11 03:43:06 -0700

    Revert "chore: Address comment 2311"
    This reverts commit ff8158eda134128744a37853390d51b72b7cae86.

--- jungle-zoo/src/state.rs
+++ jungle-zoo/src/state.rs
@@ -1,14 +1,13 @@
 use jungle_macros::Optic;
-use serde::{Deserialize, Serialize};
 
-#[derive(Optic, Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Optic, Clone, Debug, PartialEq, Eq)]
 pub struct Metabolism {
     pub energy: u64,
     pub is_hungry: bool,
     pub is_sleepy: bool,
 }
 
-#[derive(Optic, Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Optic, Clone, Debug, PartialEq)]
 pub struct Base {
     pub species: String,
     pub age: u32,
@@ -16,59 +15,59 @@ pub struct Base {
     pub metabolism: Metabolism,
 }
 
-#[derive(Optic, Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Optic, Clone, Debug, PartialEq)]
 pub struct Herbivore {
     pub base: Base,
     pub favorite_plant: String,
 }
 
-#[derive(Optic, Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Optic, Clone, Debug, PartialEq)]
 pub struct Carnivore {
     pub base: Base,
     pub favorite_meat: String,
 }
 
-#[derive(Optic, Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Optic, Clone, Debug, PartialEq)]
 pub struct Omnivore {
     pub base: Base,
     pub favorite_plant: String,
     pub favorite_meat: String,
 }
 
-#[derive(Optic, Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Optic, Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Color {
     pub r: u8,
     pub g: u8,
     pub b: u8,
 }
 
-#[derive(Optic, Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Optic, Clone, Debug, PartialEq, Eq)]
 pub struct Mammal {
     pub fur_color: Color,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum ScalePattern {
     Cycloid,
     DiamondLattice,
 }
 
-#[derive(Optic, Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Optic, Clone, Debug, PartialEq, Eq)]
 pub struct Reptile {
     pub scale_pattern: ScalePattern,
 }
 
-#[derive(Optic, Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Optic, Clone, Debug, PartialEq, Eq)]
 pub struct Swimmer {
     pub has_fins: bool,
 }
 
-#[derive(Optic, Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Optic, Clone, Debug, PartialEq, Eq)]
 pub struct Climber {
     pub is_ape: bool,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Weapon {
     Teeth,
     Claws,
@@ -76,19 +75,19 @@ pub enum Weapon {
     Mass,
 }
 
-#[derive(Optic, Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Optic, Clone, Debug, PartialEq, Eq)]
 pub struct Combatant {
     pub weapons: Weapon,
 }
 
-#[derive(Optic, Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Optic, Clone, Debug, PartialEq)]
 pub struct LionState {
     pub carnivore: Carnivore,
     pub mammal: Mammal,
     pub combatant: Combatant,
 }
 
-#[derive(Optic, Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Optic, Clone, Debug, PartialEq)]
 pub struct HippoState {
     pub herbivore: Herbivore,
     pub mammal: Mammal,
@@ -96,14 +95,14 @@ pub struct HippoState {
     pub combatant: Combatant,
 }
 
-#[derive(Optic, Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Optic, Clone, Debug, PartialEq)]
 pub struct RhinoState {
     pub herbivore: Herbivore,
     pub mammal: Mammal,
     pub combatant: Combatant,
 }
 
-#[derive(Optic, Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Optic, Clone, Debug, PartialEq)]
 pub struct CrocodileState {
     pub carnivore: Carnivore,
     pub reptile: Reptile,
@@ -111,14 +110,14 @@ pub struct CrocodileState {
     pub combatant: Combatant,
 }
 
-#[derive(Optic, Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Optic, Clone, Debug, PartialEq)]
 pub struct GiraffeState {
     pub herbivore: Herbivore,
     pub mammal: Mammal,
     pub combatant: Combatant,
 }
 
-#[derive(Optic, Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Optic, Clone, Debug, PartialEq)]
 pub struct GorillaState {
     pub omnivore: Omnivore,
     pub mammal: Mammal,
