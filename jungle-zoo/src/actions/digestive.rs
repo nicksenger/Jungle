61058b35991d3f4e33b986bd8baac06fd482fe24
The Man in the Box <mitbagent@gmail.com>
2026-05-12 03:44:18 -0700

    chore: Address comment 2497

--- jungle-zoo/src/actions/digestive.rs
+++ jungle-zoo/src/actions/digestive.rs
@@ -1,4 +1,4 @@
-use super::support::define_action;
+use super::support::{define_action, maybe_delay};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct DigestiveDependency {
@@ -35,7 +35,10 @@ define_action!(
     out = u16,
     err = String,
     act = |dependency, energy| {
-        std::future::ready(Ok(energy.saturating_add(u16::from(dependency.chew_efficiency))))
+        async move {
+            maybe_delay().await;
+            Ok(energy.saturating_add(u16::from(dependency.chew_efficiency)))
+        }
     }
 );
 
@@ -92,10 +95,13 @@ define_action!(
     out = String,
     err = String,
     act = |_dependency, opposable_thumb| {
-        if opposable_thumb {
-            std::future::ready(Ok("used stick to extract food".to_owned()))
-        } else {
-            std::future::ready(Err("no opposable thumb for tool use".to_owned()))
+        async move {
+            maybe_delay().await;
+            if opposable_thumb {
+                Ok("used stick to extract food".to_owned())
+            } else {
+                Err("no opposable thumb for tool use".to_owned())
+            }
         }
     }
 );
@@ -124,11 +130,14 @@ define_action!(
     out = u16,
     err = String,
     act = |dependency, (rind_thickness_mm, flesh_mass_g)| {
-        let peel_cost = u16::from(rind_thickness_mm).saturating_mul(2);
-        let edible = flesh_mass_g
-            .saturating_add(dependency.peel_bonus)
-            .saturating_sub(peel_cost);
-        std::future::ready(Ok(edible))
+        async move {
+            maybe_delay().await;
+            let peel_cost = u16::from(rind_thickness_mm).saturating_mul(2);
+            let edible = flesh_mass_g
+                .saturating_add(dependency.peel_bonus)
+                .saturating_sub(peel_cost);
+            Ok(edible)
+        }
     }
 );
 
