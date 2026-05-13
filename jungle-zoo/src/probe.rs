03c78fab80668c00885dc6a4cc8180125d75b213
The Man in the Box <mitbagent@gmail.com>
2026-05-13 02:45:12 -0700

    chore: Add Probe

--- jungle-zoo/src/probe.rs
+++ jungle-zoo/src/probe.rs
@@ -1,275 +1,32 @@
-macro_rules! action {
-    (
-        $name:ident,
-        $id:ty,
-        dependency = $dependency_ty:ty
-    ) => {
-        pub struct $name;
-        impl jungle_sdk::types::ActionMember for $name {}
+use jungle_sdk::typosaurus::num::consts::U255;
 
-        impl jungle_sdk::types::Action for $name {
-            type Id = jungle_sdk::types::Id<$id>;
-            type Dependency = $dependency_ty;
-            type In = ();
-            type Out = ();
-            type Err = ();
+pub struct ProbeAction;
+//impl jungle_sdk::types::ActionMember for ProbeAction {}
 
-            fn act(
-                _dependency: &Self::Dependency,
-                _input: Self::In,
-            ) -> impl std::future::Future<Output = Result<Self::Out, Self::Err>> {
-                std::future::ready(Ok(()))
-            }
-        }
+impl jungle_sdk::types::Action for ProbeAction {
+    type Id = jungle_sdk::types::Id<U255>;
+    type Dependency = ();
+    type In = ();
+    type Out = ();
+    type Err = ();
 
-        #[jungle_sdk::sdk_primitive(property = jungle_sdk::types::JungleActions)]
-        impl jungle_sdk::types::Actions for $name {
-            type List = jungle_sdk::typosaurus::collections::sp::Node<$id, $name>;
-        }
-
-        #[jungle_sdk::sdk_primitive(property = jungle_sdk::types::Ident)]
-        impl jungle_sdk::types::Identified for $name {
-            type Id = $id;
-        }
-    };
-
-    (
-        $name:ident,
-        $id:ty,
-        in = $in:ty,
-        out = $out:ty,
-        err = $err:ty,
-        act = |$dependency:ident, $input:ident| $body:expr
-    ) => {
-        pub struct $name;
-        impl jungle_sdk::types::ActionMember for $name {}
-
-        impl jungle_sdk::types::Action for $name {
-            type Id = jungle_sdk::types::Id<$id>;
-            type Dependency = ();
-            type In = $in;
-            type Out = $out;
-            type Err = $err;
-
-            fn act(
-                $dependency: &Self::Dependency,
-                $input: Self::In,
-            ) -> impl std::future::Future<Output = Result<Self::Out, Self::Err>> {
-                $body
-            }
-        }
-    };
-
-    ($name:ident, $id:ty) => {
-        pub struct $name;
-        impl jungle_sdk::types::ActionMember for $name {}
-
-        impl jungle_sdk::types::Action for $name {
-            type Id = jungle_sdk::types::Id<$id>;
-            type Dependency = ();
-            type In = ();
-            type Out = ();
-            type Err = ();
-
-            fn act(
-                _dependency: &Self::Dependency,
-                _input: Self::In,
-            ) -> impl std::future::Future<Output = Result<Self::Out, Self::Err>> {
-                std::future::ready(Ok(()))
-            }
-        }
-
-        #[jungle_sdk::sdk_primitive(property = jungle_sdk::types::JungleActions)]
-        impl jungle_sdk::types::Actions for $name {
-            type List = jungle_sdk::typosaurus::collections::sp::Node<$id, $name>;
-        }
-
-        #[jungle_sdk::sdk_primitive(property = jungle_sdk::types::Ident)]
-        impl jungle_sdk::types::Identified for $name {
-            type Id = $id;
-        }
-    };
+    fn act(
+        _dependency: &Self::Dependency,
+        _input: Self::In,
+    ) -> impl std::future::Future<Output = Result<Self::Out, Self::Err>> {
+        std::future::ready(Ok(()))
+    }
 }
 
-macro_rules! animal {
-    ($name:ident, $id:ty, state = $state:ty, journey = $journey:ty, observe = true, perturb = true) => {
-        pub struct $name;
+//#[jungle_sdk::sdk_primitive(property = jungle_sdk::types::JungleActions)]
+//impl jungle_sdk::types::Actions for ProbeAction {
+//    type List = jungle_sdk::typosaurus::collections::sp::Node<U255, ProbeAction>;
+//}
 
-        impl jungle_sdk::types::Animal for $name {
-            type Id = jungle_sdk::types::Id<$id>;
-            type Generation = jungle_sdk::typosaurus::num::consts::U0;
-            type State = $state;
-            type Seed = $state;
-            type Journey = $journey;
-        }
-
-        impl jungle_sdk::types::AnimalObservation for $name {
-            type Adapter = jungle_sdk::types::ObserveObservation;
-        }
-
-        impl jungle_sdk::types::AnimalPerturbation for $name {
-            type Adapter = jungle_sdk::types::TraitPerturbation;
-        }
-    };
-
-    ($name:ident, $id:ty, state = $state:ty, journey = $journey:ty, perturb = true) => {
-        pub struct $name;
-
-        impl jungle_sdk::types::Animal for $name {
-            type Id = jungle_sdk::types::Id<$id>;
-            type Generation = jungle_sdk::typosaurus::num::consts::U0;
-            type State = $state;
-            type Seed = $state;
-            type Journey = $journey;
-        }
-
-        impl jungle_sdk::types::AnimalObservation for $name {
-            type Adapter = jungle_sdk::types::NoopObservation;
-        }
-
-        impl jungle_sdk::types::AnimalPerturbation for $name {
-            type Adapter = jungle_sdk::types::TraitPerturbation;
-        }
-    };
-
-    ($name:ident, $id:ty, state = $state:ty, journey = $journey:ty, observe = true) => {
-        pub struct $name;
-
-        impl jungle_sdk::types::Animal for $name {
-            type Id = jungle_sdk::types::Id<$id>;
-            type Generation = jungle_sdk::typosaurus::num::consts::U0;
-            type State = $state;
-            type Seed = $state;
-            type Journey = $journey;
-        }
-
-        impl jungle_sdk::types::AnimalObservation for $name {
-            type Adapter = jungle_sdk::types::ObserveObservation;
-        }
-
-        impl jungle_sdk::types::AnimalPerturbation for $name {
-            type Adapter = jungle_sdk::types::NoopPerturbation;
-        }
-    };
-
-    ($name:ident, $id:ty, state = $state:ty, journey = $journey:ty) => {
-        pub struct $name;
-
-        impl jungle_sdk::types::Animal for $name {
-            type Id = jungle_sdk::types::Id<$id>;
-            type Generation = jungle_sdk::typosaurus::num::consts::U0;
-            type State = $state;
-            type Seed = $state;
-            type Journey = $journey;
-        }
-
-        impl jungle_sdk::types::AnimalObservation for $name {
-            type Adapter = jungle_sdk::types::NoopObservation;
-        }
-
-        impl jungle_sdk::types::AnimalPerturbation for $name {
-            type Adapter = jungle_sdk::types::NoopPerturbation;
-        }
-    };
-
-    ($name:ident, $id:ty, journey = $journey:ty) => {
-        animal!($name, $id, state = (), journey = $journey);
-    };
-
-    ($name:ident, $id:ty, $journey:ty) => {
-        animal!($name, $id, SharedState, $journey);
-    };
-
-    ($name:ident, $id:ty, $state:ty, $journey:ty) => {
-        pub struct $name;
-        impl jungle_sdk::types::AnimalMember for $name {}
-
-        impl jungle_sdk::types::Animal for $name {
-            type Id = jungle_sdk::types::Id<$id>;
-            type Generation = jungle_sdk::typosaurus::num::consts::U0;
-            type State = $state;
-            type Seed = $state;
-            type Journey = $journey;
-        }
-
-        impl jungle_sdk::types::AnimalObservation for $name {
-            type Adapter = jungle_sdk::types::NoopObservation;
-        }
-
-        impl jungle_sdk::types::AnimalPerturbation for $name {
-            type Adapter = jungle_sdk::types::NoopPerturbation;
-        }
-
-        #[jungle_sdk::sdk_primitive(property = jungle_sdk::types::JungleAnimals)]
-        impl jungle_sdk::types::Animals for $name {
-            type List = jungle_sdk::typosaurus::collections::sp::Node<$id, $name>;
-        }
-
-        #[jungle_sdk::sdk_primitive(property = jungle_sdk::types::Ident)]
-        impl jungle_sdk::types::Identified for $name {
-            type Id = $id;
-        }
-    };
-
-    ($name:ident, $id:ty, $state:ty, $journey:ty, observe = true, perturb = true) => {
-        pub struct $name;
-        impl jungle_sdk::types::AnimalMember for $name {}
-
-        impl jungle_sdk::types::Animal for $name {
-            type Id = jungle_sdk::types::Id<$id>;
-            type Generation = jungle_sdk::typosaurus::num::consts::U0;
-            type State = $state;
-            type Seed = $state;
-            type Journey = $journey;
-        }
-
-        impl jungle_sdk::types::AnimalObservation for $name {
-            type Adapter = jungle_sdk::types::ObserveObservation;
-        }
-
-        impl jungle_sdk::types::AnimalPerturbation for $name {
-            type Adapter = jungle_sdk::types::TraitPerturbation;
-        }
-
-        #[jungle_sdk::sdk_primitive(property = jungle_sdk::types::JungleAnimals)]
-        impl jungle_sdk::types::Animals for $name {
-            type List = jungle_sdk::typosaurus::collections::sp::Node<$id, $name>;
-        }
-
-        #[jungle_sdk::sdk_primitive(property = jungle_sdk::types::Ident)]
-        impl jungle_sdk::types::Identified for $name {
-            type Id = $id;
-        }
-    };
-
-    ($name:ident, $id:ty, $state:ty, $journey:ty, observe = true) => {
-        pub struct $name;
-        impl jungle_sdk::types::AnimalMember for $name {}
-
-        impl jungle_sdk::types::Animal for $name {
-            type Id = jungle_sdk::types::Id<$id>;
-            type Generation = jungle_sdk::typosaurus::num::consts::U0;
-            type State = $state;
-            type Seed = $state;
-            type Journey = $journey;
-        }
-
-        impl jungle_sdk::types::AnimalObservation for $name {
-            type Adapter = jungle_sdk::types::ObserveObservation;
-        }
-
-        impl jungle_sdk::types::AnimalPerturbation for $name {
-            type Adapter = jungle_sdk::types::NoopPerturbation;
-        }
-
-        #[jungle_sdk::sdk_primitive(property = jungle_sdk::types::JungleAnimals)]
-        impl jungle_sdk::types::Animals for $name {
-            type List = jungle_sdk::typosaurus::collections::sp::Node<$id, $name>;
-        }
-    };
-}
-
-action!(ProbeAction, jungle_sdk::typosaurus::num::consts::U255);
+//#[jungle_sdk::sdk_primitive(property = jungle_sdk::types::Ident)]
+//impl jungle_sdk::types::Identified for ProbeAction {
+//    type Id = U255;
+//}
 
 pub struct ProbeStep;
 impl jungle_sdk::types::Pulse<ProbeAnimal> for ProbeStep {
@@ -281,7 +38,8 @@ impl jungle_sdk::types::Pulse<ProbeAnimal> for ProbeStep {
     fn emit(
         _state: &<ProbeAnimal as jungle_sdk::types::Animal>::State,
         _input: Self::CarryIn,
-    ) -> <Self::Action as jungle_sdk::types::Action>::In {}
+    ) -> <Self::Action as jungle_sdk::types::Action>::In {
+    }
 
     fn absorb(
         _state: &mut <ProbeAnimal as jungle_sdk::types::Animal>::State,
@@ -293,12 +51,34 @@ impl jungle_sdk::types::Pulse<ProbeAnimal> for ProbeStep {
 #[derive(jungle_sdk::Journey)]
 pub struct ProbeJourney(jungle_sdk::types::Step<ProbeAnimal, ProbeStep>);
 
-animal!(
-    ProbeAnimal,
-    jungle_sdk::typosaurus::num::consts::U255,
-    (),
-    ProbeJourney
-);
+pub struct ProbeAnimal;
+impl jungle_sdk::types::AnimalMember for ProbeAnimal {}
+
+impl jungle_sdk::types::Animal for ProbeAnimal {
+    type Id = jungle_sdk::types::Id<U255>;
+    type Generation = jungle_sdk::typosaurus::num::consts::U0;
+    type State = ();
+    type Seed = ();
+    type Journey = ProbeJourney;
+}
+
+impl jungle_sdk::types::AnimalObservation for ProbeAnimal {
+    type Adapter = jungle_sdk::types::NoopObservation;
+}
+
+impl jungle_sdk::types::AnimalPerturbation for ProbeAnimal {
+    type Adapter = jungle_sdk::types::NoopPerturbation;
+}
+
+#[jungle_sdk::sdk_primitive(property = jungle_sdk::types::JungleAnimals)]
+impl jungle_sdk::types::Animals for ProbeAnimal {
+    type List = jungle_sdk::typosaurus::collections::sp::Node<U255, ProbeAnimal>;
+}
+
+#[jungle_sdk::sdk_primitive(property = jungle_sdk::types::Ident)]
+impl jungle_sdk::types::Identified for ProbeAnimal {
+    type Id = U255;
+}
 
 #[derive(jungle_sdk::Animals)]
 pub struct ProbeZooAnimals(ProbeAnimal);
