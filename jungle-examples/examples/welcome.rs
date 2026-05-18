0059d9a2db5c234d365cc65b5d82586daf9b43cd
The Man in the Box <mitbagent@gmail.com>
2026-05-18 02:15:03 -0700

    chore: Address comment 4187

---
+++ jungle-examples/examples/welcome.rs
@@ -0,0 +1,14 @@
+use cpal::traits::{DeviceTrait, HostTrait};
+
+fn main() {
+    let _viewer = jungle_viewer::JungleViewerBuilder::new().title("Welcome Example");
+
+    let host = cpal::default_host();
+    let device_name = host
+        .default_output_device()
+        .and_then(|device| device.description().ok().map(|description| description.name().to_string()))
+        .unwrap_or_else(|| "none".to_string());
+
+    println!("welcome example");
+    println!("default audio output device: {device_name}");
+}
