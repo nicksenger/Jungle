6fe679fb750261dd1454eb38e3bc4f5a14504e1e
The Man in the Box <mitbagent@gmail.com>
2026-05-21 00:18:04 -0700

    chore: Start pick action

--- jungle-examples/examples/welcome/animals/rhythm_guitarist.rs
+++ jungle-examples/examples/welcome/animals/rhythm_guitarist.rs
@@ -1,8 +1,10 @@
+use jungle_sdk::effect;
 use jungle_sdk::prelude::*;
 use num::U255;
 use std::time::Duration;
 
 use crate::flow;
+use crate::score::Kind;
 
 pub type RhythmGuitaristState = flow::RhythmGuitarIntroState;
 pub type RhythmGuitaristSeed = ();
@@ -15,3 +17,30 @@ impl Animal for RhythmGuitarist {
     type Seed = RhythmGuitaristSeed;
     type Journey = flow::RhythmGuitaristJourney;
 }
+
+pub struct Pick<const NOTE: u8, const TICKS: u8>(NOTE, TICKS);
+
+//#[effect(id = 500)]
+//impl<const NOTE: u8, const TICKS: u8> Effect<WelcomeEcosystem> for Pick<NOTE, TICKS> {
+//    type In = ();
+//    type Out = ();
+//    type Err = String;
+//
+//    async fn effect(jungle: &WelcomeEcosystem, note: Self::In) -> Result<Self::Out, Self::Err> {
+//        let bpm = jungle.bpm();
+//        let playable_note = Note {
+//            n_midi: NOTE,
+//            amplitude_multiplier: 0.5,
+//            pan: 0.5,
+//            duration: std::time::Duration::from_secs_f32(duration_beats * seconds_per_beat),
+//            velocity: 37.0 / 127.0,
+//            expression: None,
+//            articulation: ElectricGuitarArticulation::RhythmSustained,
+//        };
+//        jungle
+//            .rhythm_guitar()
+//            .play(playable_note)
+//            .await
+//            .map_err(|err| err.to_string())
+//    }
+//}
