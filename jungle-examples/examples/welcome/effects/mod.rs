da631bb7d4582575e1760feccafafb88eb7b04d1
The Man in the Box <mitbagent@gmail.com>
2026-05-21 02:12:38 -0700

    chore: Address comment 4767

--- jungle-examples/examples/welcome/effects/mod.rs
+++ jungle-examples/examples/welcome/effects/mod.rs
@@ -26,6 +26,52 @@ pub struct Dyad<
     const REST_TICK: u8,
 >(PhantomData<(I, A)>);
 
+pub struct Triad<
+    I: Instrument<Articulation = A>,
+    A: RhythmArticulation,
+    const NOTE_ONE: u8,
+    const NOTE_TWO: u8,
+    const NOTE_THREE: u8,
+    const NOTE_TICK: u8,
+    const REST_TICK: u8,
+>(PhantomData<(I, A)>);
+
+pub struct Tetrad<
+    I: Instrument<Articulation = A>,
+    A: RhythmArticulation,
+    const NOTE_ONE: u8,
+    const NOTE_TWO: u8,
+    const NOTE_THREE: u8,
+    const NOTE_FOUR: u8,
+    const NOTE_TICK: u8,
+    const REST_TICK: u8,
+>(PhantomData<(I, A)>);
+
+pub struct Pentad<
+    I: Instrument<Articulation = A>,
+    A: RhythmArticulation,
+    const NOTE_ONE: u8,
+    const NOTE_TWO: u8,
+    const NOTE_THREE: u8,
+    const NOTE_FOUR: u8,
+    const NOTE_FIVE: u8,
+    const NOTE_TICK: u8,
+    const REST_TICK: u8,
+>(PhantomData<(I, A)>);
+
+pub struct Hexad<
+    I: Instrument<Articulation = A>,
+    A: RhythmArticulation,
+    const NOTE_ONE: u8,
+    const NOTE_TWO: u8,
+    const NOTE_THREE: u8,
+    const NOTE_FOUR: u8,
+    const NOTE_FIVE: u8,
+    const NOTE_SIX: u8,
+    const NOTE_TICK: u8,
+    const REST_TICK: u8,
+>(PhantomData<(I, A)>);
+
 pub trait RhythmArticulation: Copy + Into<ElectricGuitarArticulation> {
     fn rhythm_sustained() -> Self;
 }
@@ -130,6 +176,397 @@ where
             );
             first.map_err(|err| err.to_string())?;
             second.map_err(|err| err.to_string())?;
+        }
+
+        let sleep_for = rest_duration.saturating_sub(phase_offset);
+        if !sleep_for.is_zero() {
+            tokio::time::sleep(sleep_for).await;
+        }
+
+        Ok(())
+    }
+}
+
+#[effect(id = 502)]
+impl<
+    I,
+    A,
+    const NOTE_ONE: u8,
+    const NOTE_TWO: u8,
+    const NOTE_THREE: u8,
+    const NOTE_TICK: u8,
+    const REST_TICK: u8,
+> jungle_sdk::prelude::Effect<WelcomeEcosystem>
+    for Triad<I, A, NOTE_ONE, NOTE_TWO, NOTE_THREE, NOTE_TICK, REST_TICK>
+where
+    I: Instrument<Articulation = A>,
+    A: RhythmArticulation,
+{
+    type In = ();
+    type Out = ();
+    type Err = String;
+
+    async fn effect(jungle: &WelcomeEcosystem, _note: Self::In) -> Result<Self::Out, Self::Err> {
+        let metronome = jungle.metronome();
+        let beat_duration = metronome.beat_duration();
+        let tick_duration = beat_duration.div_f32(TICKS_PER_BEAT as f32);
+        let rest_duration = duration_for_ticks(tick_duration, REST_TICK as u32);
+        let late_note_drop_threshold = late_note_drop_threshold(beat_duration);
+        let phase_offset = current_phase_offset(metronome, rest_duration);
+
+        let note_duration = duration_for_ticks(tick_duration, NOTE_TICK as u32);
+        let articulation = A::rhythm_sustained().into();
+        let playable_note_one = Note::<ElectricGuitarArticulation> {
+            n_midi: NOTE_ONE,
+            amplitude_multiplier: 0.5,
+            pan: 0.5,
+            duration: note_duration,
+            velocity: 37.0 / 127.0,
+            expression: None,
+            articulation,
+        };
+        let playable_note_two = Note::<ElectricGuitarArticulation> {
+            n_midi: NOTE_TWO,
+            amplitude_multiplier: 0.5,
+            pan: 0.5,
+            duration: note_duration,
+            velocity: 37.0 / 127.0,
+            expression: None,
+            articulation,
+        };
+        let playable_note_three = Note::<ElectricGuitarArticulation> {
+            n_midi: NOTE_THREE,
+            amplitude_multiplier: 0.5,
+            pan: 0.5,
+            duration: note_duration,
+            velocity: 37.0 / 127.0,
+            expression: None,
+            articulation,
+        };
+
+        if phase_offset <= late_note_drop_threshold {
+            let (first, second, third) = tokio::join!(
+                jungle.rhythm_guitar().play(playable_note_one),
+                jungle.rhythm_guitar().play(playable_note_two),
+                jungle.rhythm_guitar().play(playable_note_three)
+            );
+            first.map_err(|err| err.to_string())?;
+            second.map_err(|err| err.to_string())?;
+            third.map_err(|err| err.to_string())?;
+        }
+
+        let sleep_for = rest_duration.saturating_sub(phase_offset);
+        if !sleep_for.is_zero() {
+            tokio::time::sleep(sleep_for).await;
+        }
+
+        Ok(())
+    }
+}
+
+#[effect(id = 504)]
+impl<
+    I,
+    A,
+    const NOTE_ONE: u8,
+    const NOTE_TWO: u8,
+    const NOTE_THREE: u8,
+    const NOTE_FOUR: u8,
+    const NOTE_TICK: u8,
+    const REST_TICK: u8,
+> jungle_sdk::prelude::Effect<WelcomeEcosystem>
+    for Tetrad<I, A, NOTE_ONE, NOTE_TWO, NOTE_THREE, NOTE_FOUR, NOTE_TICK, REST_TICK>
+where
+    I: Instrument<Articulation = A>,
+    A: RhythmArticulation,
+{
+    type In = ();
+    type Out = ();
+    type Err = String;
+
+    async fn effect(jungle: &WelcomeEcosystem, _note: Self::In) -> Result<Self::Out, Self::Err> {
+        let metronome = jungle.metronome();
+        let beat_duration = metronome.beat_duration();
+        let tick_duration = beat_duration.div_f32(TICKS_PER_BEAT as f32);
+        let rest_duration = duration_for_ticks(tick_duration, REST_TICK as u32);
+        let late_note_drop_threshold = late_note_drop_threshold(beat_duration);
+        let phase_offset = current_phase_offset(metronome, rest_duration);
+
+        let note_duration = duration_for_ticks(tick_duration, NOTE_TICK as u32);
+        let articulation = A::rhythm_sustained().into();
+        let playable_note_one = Note::<ElectricGuitarArticulation> {
+            n_midi: NOTE_ONE,
+            amplitude_multiplier: 0.5,
+            pan: 0.5,
+            duration: note_duration,
+            velocity: 37.0 / 127.0,
+            expression: None,
+            articulation,
+        };
+        let playable_note_two = Note::<ElectricGuitarArticulation> {
+            n_midi: NOTE_TWO,
+            amplitude_multiplier: 0.5,
+            pan: 0.5,
+            duration: note_duration,
+            velocity: 37.0 / 127.0,
+            expression: None,
+            articulation,
+        };
+        let playable_note_three = Note::<ElectricGuitarArticulation> {
+            n_midi: NOTE_THREE,
+            amplitude_multiplier: 0.5,
+            pan: 0.5,
+            duration: note_duration,
+            velocity: 37.0 / 127.0,
+            expression: None,
+            articulation,
+        };
+        let playable_note_four = Note::<ElectricGuitarArticulation> {
+            n_midi: NOTE_FOUR,
+            amplitude_multiplier: 0.5,
+            pan: 0.5,
+            duration: note_duration,
+            velocity: 37.0 / 127.0,
+            expression: None,
+            articulation,
+        };
+
+        if phase_offset <= late_note_drop_threshold {
+            let (first, second, third, fourth) = tokio::join!(
+                jungle.rhythm_guitar().play(playable_note_one),
+                jungle.rhythm_guitar().play(playable_note_two),
+                jungle.rhythm_guitar().play(playable_note_three),
+                jungle.rhythm_guitar().play(playable_note_four)
+            );
+            first.map_err(|err| err.to_string())?;
+            second.map_err(|err| err.to_string())?;
+            third.map_err(|err| err.to_string())?;
+            fourth.map_err(|err| err.to_string())?;
+        }
+
+        let sleep_for = rest_duration.saturating_sub(phase_offset);
+        if !sleep_for.is_zero() {
+            tokio::time::sleep(sleep_for).await;
+        }
+
+        Ok(())
+    }
+}
+
+#[effect(id = 505)]
+impl<
+    I,
+    A,
+    const NOTE_ONE: u8,
+    const NOTE_TWO: u8,
+    const NOTE_THREE: u8,
+    const NOTE_FOUR: u8,
+    const NOTE_FIVE: u8,
+    const NOTE_TICK: u8,
+    const REST_TICK: u8,
+> jungle_sdk::prelude::Effect<WelcomeEcosystem>
+    for Pentad<I, A, NOTE_ONE, NOTE_TWO, NOTE_THREE, NOTE_FOUR, NOTE_FIVE, NOTE_TICK, REST_TICK>
+where
+    I: Instrument<Articulation = A>,
+    A: RhythmArticulation,
+{
+    type In = ();
+    type Out = ();
+    type Err = String;
+
+    async fn effect(jungle: &WelcomeEcosystem, _note: Self::In) -> Result<Self::Out, Self::Err> {
+        let metronome = jungle.metronome();
+        let beat_duration = metronome.beat_duration();
+        let tick_duration = beat_duration.div_f32(TICKS_PER_BEAT as f32);
+        let rest_duration = duration_for_ticks(tick_duration, REST_TICK as u32);
+        let late_note_drop_threshold = late_note_drop_threshold(beat_duration);
+        let phase_offset = current_phase_offset(metronome, rest_duration);
+
+        let note_duration = duration_for_ticks(tick_duration, NOTE_TICK as u32);
+        let articulation = A::rhythm_sustained().into();
+        let playable_note_one = Note::<ElectricGuitarArticulation> {
+            n_midi: NOTE_ONE,
+            amplitude_multiplier: 0.5,
+            pan: 0.5,
+            duration: note_duration,
+            velocity: 37.0 / 127.0,
+            expression: None,
+            articulation,
+        };
+        let playable_note_two = Note::<ElectricGuitarArticulation> {
+            n_midi: NOTE_TWO,
+            amplitude_multiplier: 0.5,
+            pan: 0.5,
+            duration: note_duration,
+            velocity: 37.0 / 127.0,
+            expression: None,
+            articulation,
+        };
+        let playable_note_three = Note::<ElectricGuitarArticulation> {
+            n_midi: NOTE_THREE,
+            amplitude_multiplier: 0.5,
+            pan: 0.5,
+            duration: note_duration,
+            velocity: 37.0 / 127.0,
+            expression: None,
+            articulation,
+        };
+        let playable_note_four = Note::<ElectricGuitarArticulation> {
+            n_midi: NOTE_FOUR,
+            amplitude_multiplier: 0.5,
+            pan: 0.5,
+            duration: note_duration,
+            velocity: 37.0 / 127.0,
+            expression: None,
+            articulation,
+        };
+        let playable_note_five = Note::<ElectricGuitarArticulation> {
+            n_midi: NOTE_FIVE,
+            amplitude_multiplier: 0.5,
+            pan: 0.5,
+            duration: note_duration,
+            velocity: 37.0 / 127.0,
+            expression: None,
+            articulation,
+        };
+
+        if phase_offset <= late_note_drop_threshold {
+            let (first, second, third, fourth, fifth) = tokio::join!(
+                jungle.rhythm_guitar().play(playable_note_one),
+                jungle.rhythm_guitar().play(playable_note_two),
+                jungle.rhythm_guitar().play(playable_note_three),
+                jungle.rhythm_guitar().play(playable_note_four),
+                jungle.rhythm_guitar().play(playable_note_five)
+            );
+            first.map_err(|err| err.to_string())?;
+            second.map_err(|err| err.to_string())?;
+            third.map_err(|err| err.to_string())?;
+            fourth.map_err(|err| err.to_string())?;
+            fifth.map_err(|err| err.to_string())?;
+        }
+
+        let sleep_for = rest_duration.saturating_sub(phase_offset);
+        if !sleep_for.is_zero() {
+            tokio::time::sleep(sleep_for).await;
+        }
+
+        Ok(())
+    }
+}
+
+#[effect(id = 506)]
+impl<
+    I,
+    A,
+    const NOTE_ONE: u8,
+    const NOTE_TWO: u8,
+    const NOTE_THREE: u8,
+    const NOTE_FOUR: u8,
+    const NOTE_FIVE: u8,
+    const NOTE_SIX: u8,
+    const NOTE_TICK: u8,
+    const REST_TICK: u8,
+> jungle_sdk::prelude::Effect<WelcomeEcosystem>
+    for Hexad<
+        I,
+        A,
+        NOTE_ONE,
+        NOTE_TWO,
+        NOTE_THREE,
+        NOTE_FOUR,
+        NOTE_FIVE,
+        NOTE_SIX,
+        NOTE_TICK,
+        REST_TICK,
+    >
+where
+    I: Instrument<Articulation = A>,
+    A: RhythmArticulation,
+{
+    type In = ();
+    type Out = ();
+    type Err = String;
+
+    async fn effect(jungle: &WelcomeEcosystem, _note: Self::In) -> Result<Self::Out, Self::Err> {
+        let metronome = jungle.metronome();
+        let beat_duration = metronome.beat_duration();
+        let tick_duration = beat_duration.div_f32(TICKS_PER_BEAT as f32);
+        let rest_duration = duration_for_ticks(tick_duration, REST_TICK as u32);
+        let late_note_drop_threshold = late_note_drop_threshold(beat_duration);
+        let phase_offset = current_phase_offset(metronome, rest_duration);
+
+        let note_duration = duration_for_ticks(tick_duration, NOTE_TICK as u32);
+        let articulation = A::rhythm_sustained().into();
+        let playable_note_one = Note::<ElectricGuitarArticulation> {
+            n_midi: NOTE_ONE,
+            amplitude_multiplier: 0.5,
+            pan: 0.5,
+            duration: note_duration,
+            velocity: 37.0 / 127.0,
+            expression: None,
+            articulation,
+        };
+        let playable_note_two = Note::<ElectricGuitarArticulation> {
+            n_midi: NOTE_TWO,
+            amplitude_multiplier: 0.5,
+            pan: 0.5,
+            duration: note_duration,
+            velocity: 37.0 / 127.0,
+            expression: None,
+            articulation,
+        };
+        let playable_note_three = Note::<ElectricGuitarArticulation> {
+            n_midi: NOTE_THREE,
+            amplitude_multiplier: 0.5,
+            pan: 0.5,
+            duration: note_duration,
+            velocity: 37.0 / 127.0,
+            expression: None,
+            articulation,
+        };
+        let playable_note_four = Note::<ElectricGuitarArticulation> {
+            n_midi: NOTE_FOUR,
+            amplitude_multiplier: 0.5,
+            pan: 0.5,
+            duration: note_duration,
+            velocity: 37.0 / 127.0,
+            expression: None,
+            articulation,
+        };
+        let playable_note_five = Note::<ElectricGuitarArticulation> {
+            n_midi: NOTE_FIVE,
+            amplitude_multiplier: 0.5,
+            pan: 0.5,
+            duration: note_duration,
+            velocity: 37.0 / 127.0,
+            expression: None,
+            articulation,
+        };
+        let playable_note_six = Note::<ElectricGuitarArticulation> {
+            n_midi: NOTE_SIX,
+            amplitude_multiplier: 0.5,
+            pan: 0.5,
+            duration: note_duration,
+            velocity: 37.0 / 127.0,
+            expression: None,
+            articulation,
+        };
+
+        if phase_offset <= late_note_drop_threshold {
+            let (first, second, third, fourth, fifth, sixth) = tokio::join!(
+                jungle.rhythm_guitar().play(playable_note_one),
+                jungle.rhythm_guitar().play(playable_note_two),
+                jungle.rhythm_guitar().play(playable_note_three),
+                jungle.rhythm_guitar().play(playable_note_four),
+                jungle.rhythm_guitar().play(playable_note_five),
+                jungle.rhythm_guitar().play(playable_note_six)
+            );
+            first.map_err(|err| err.to_string())?;
+            second.map_err(|err| err.to_string())?;
+            third.map_err(|err| err.to_string())?;
+            fourth.map_err(|err| err.to_string())?;
+            fifth.map_err(|err| err.to_string())?;
+            sixth.map_err(|err| err.to_string())?;
         }
 
         let sleep_for = rest_duration.saturating_sub(phase_offset);
