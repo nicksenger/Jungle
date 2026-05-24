6dce85c5d2e6bbb22fb3692a2f7a2279bb4a5fd1
The Man in the Box <mitbagent@gmail.com>
2026-05-24 00:57:52 -0700

    chore: Address comment 5363

--- jungle-test/src/tests/select_join.rs
+++ jungle-test/src/tests/select_join.rs
@@ -788,190 +788,21 @@ impl Act for FlattenJoinedUnitTupleSpec {
     }
 }
 
-pub struct FlattenConditionalEitherSpec;
+pub struct LocalTailStubSpec;
 #[jungle::act]
-impl Act for FlattenConditionalEitherSpec {
-    type Effect = Noop;
-    type Input = Either<(), ()>;
-    type Output = ();
-
-    fn emit(_state: &SelectJoinState, _input: Self::Input) -> () {}
-
-    fn absorb(
-        _state: &mut SelectJoinState,
-        output: EffectCompletion<Self::Effect>,
-    ) -> Self::Output {
-        output.expect("flatten conditional either should succeed");
-    }
-}
-
-pub struct LocalTailStub1Spec;
-#[jungle::act]
-impl Act for LocalTailStub1Spec {
+impl Act for LocalTailStubSpec {
     type Effect = TimedValueEffect;
-    type Input = ();
-    type Output = ();
+    type Input = Either<(), ()>;
+    type Output = Either<(), ()>;
 
     fn emit(_state: &SelectJoinState, _input: Self::Input) -> (u64, i32) {
         (0, 5)
     }
 
     fn absorb(state: &mut SelectJoinState, output: EffectCompletion<Self::Effect>) -> Self::Output {
-        output.expect("local tail stub 1 should succeed");
+        output.expect("local tail stub should succeed");
         state.joined_sum = state.joined_sum.saturating_add(1);
-    }
-}
-
-pub struct LocalTailStub2Spec;
-#[jungle::act]
-impl Act for LocalTailStub2Spec {
-    type Effect = TimedValueEffect;
-    type Input = ();
-    type Output = ();
-
-    fn emit(_state: &SelectJoinState, _input: Self::Input) -> (u64, i32) {
-        (0, 6)
-    }
-
-    fn absorb(state: &mut SelectJoinState, output: EffectCompletion<Self::Effect>) -> Self::Output {
-        output.expect("local tail stub 2 should succeed");
-        state.joined_sum = state.joined_sum.saturating_add(1);
-    }
-}
-
-pub struct LocalTailStub3Spec;
-#[jungle::act]
-impl Act for LocalTailStub3Spec {
-    type Effect = TimedValueEffect;
-    type Input = ();
-    type Output = ();
-
-    fn emit(_state: &SelectJoinState, _input: Self::Input) -> (u64, i32) {
-        (0, 7)
-    }
-
-    fn absorb(state: &mut SelectJoinState, output: EffectCompletion<Self::Effect>) -> Self::Output {
-        output.expect("local tail stub 3 should succeed");
-        state.joined_sum = state.joined_sum.saturating_add(1);
-    }
-}
-
-pub struct LocalTailStub4Spec;
-#[jungle::act]
-impl Act for LocalTailStub4Spec {
-    type Effect = TimedValueEffect;
-    type Input = ();
-    type Output = ();
-
-    fn emit(_state: &SelectJoinState, _input: Self::Input) -> (u64, i32) {
-        (0, 8)
-    }
-
-    fn absorb(state: &mut SelectJoinState, output: EffectCompletion<Self::Effect>) -> Self::Output {
-        output.expect("local tail stub 4 should succeed");
-        state.joined_sum = state.joined_sum.saturating_add(1);
-    }
-}
-
-pub struct LocalTailStub5Spec;
-#[jungle::act]
-impl Act for LocalTailStub5Spec {
-    type Effect = TimedValueEffect;
-    type Input = ();
-    type Output = ();
-
-    fn emit(_state: &SelectJoinState, _input: Self::Input) -> (u64, i32) {
-        (0, 9)
-    }
-
-    fn absorb(state: &mut SelectJoinState, output: EffectCompletion<Self::Effect>) -> Self::Output {
-        output.expect("local tail stub 5 should succeed");
-        state.joined_sum = state.joined_sum.saturating_add(1);
-    }
-}
-
-pub struct LocalTailStub6Spec;
-#[jungle::act]
-impl Act for LocalTailStub6Spec {
-    type Effect = TimedValueEffect;
-    type Input = ();
-    type Output = ();
-
-    fn emit(_state: &SelectJoinState, _input: Self::Input) -> (u64, i32) {
-        (0, 10)
-    }
-
-    fn absorb(state: &mut SelectJoinState, output: EffectCompletion<Self::Effect>) -> Self::Output {
-        output.expect("local tail stub 6 should succeed");
-        state.joined_sum = state.joined_sum.saturating_add(1);
-    }
-}
-
-pub struct LocalTailStub7Spec;
-#[jungle::act]
-impl Act for LocalTailStub7Spec {
-    type Effect = TimedValueEffect;
-    type Input = ();
-    type Output = ();
-
-    fn emit(_state: &SelectJoinState, _input: Self::Input) -> (u64, i32) {
-        (0, 11)
-    }
-
-    fn absorb(state: &mut SelectJoinState, output: EffectCompletion<Self::Effect>) -> Self::Output {
-        output.expect("local tail stub 7 should succeed");
-        state.joined_sum = state.joined_sum.saturating_add(1);
-    }
-}
-
-pub struct LocalTailStub8Spec;
-#[jungle::act]
-impl Act for LocalTailStub8Spec {
-    type Effect = TimedValueEffect;
-    type Input = ();
-    type Output = ();
-
-    fn emit(_state: &SelectJoinState, _input: Self::Input) -> (u64, i32) {
-        (0, 12)
-    }
-
-    fn absorb(state: &mut SelectJoinState, output: EffectCompletion<Self::Effect>) -> Self::Output {
-        output.expect("local tail stub 8 should succeed");
-        state.joined_sum = state.joined_sum.saturating_add(1);
-    }
-}
-
-pub struct LocalTailStub9Spec;
-#[jungle::act]
-impl Act for LocalTailStub9Spec {
-    type Effect = TimedValueEffect;
-    type Input = ();
-    type Output = ();
-
-    fn emit(_state: &SelectJoinState, _input: Self::Input) -> (u64, i32) {
-        (0, 13)
-    }
-
-    fn absorb(state: &mut SelectJoinState, output: EffectCompletion<Self::Effect>) -> Self::Output {
-        output.expect("local tail stub 9 should succeed");
-        state.joined_sum = state.joined_sum.saturating_add(1);
-    }
-}
-
-pub struct LocalTailStub10Spec;
-#[jungle::act]
-impl Act for LocalTailStub10Spec {
-    type Effect = TimedValueEffect;
-    type Input = ();
-    type Output = ();
-
-    fn emit(_state: &SelectJoinState, _input: Self::Input) -> (u64, i32) {
-        (0, 14)
-    }
-
-    fn absorb(state: &mut SelectJoinState, output: EffectCompletion<Self::Effect>) -> Self::Output {
-        output.expect("local tail stub 10 should succeed");
-        state.joined_sum = state.joined_sum.saturating_add(1);
+        Either::Left(())
     }
 }
 
@@ -988,42 +819,18 @@ pub struct LocalConditionalRightBranch(
 );
 
 #[derive(Flow)]
-pub struct LocalTail10Flow(Step<LocalTailStub10Spec>);
-
-#[derive(Flow)]
-pub struct LocalTail9Flow(Step<LocalTailStub9Spec>, LocalTail10Flow);
-
-#[derive(Flow)]
-pub struct LocalTail8Flow(Step<LocalTailStub8Spec>, LocalTail9Flow);
-
-#[derive(Flow)]
-pub struct LocalTail7Flow(Step<LocalTailStub7Spec>, LocalTail8Flow);
-
-#[derive(Flow)]
-pub struct LocalTail6Flow(Step<LocalTailStub6Spec>, LocalTail7Flow);
-
-#[derive(Flow)]
-pub struct LocalTail5Flow(Step<LocalTailStub5Spec>, LocalTail6Flow);
-
-#[derive(Flow)]
-pub struct LocalTail4Flow(Step<LocalTailStub4Spec>, LocalTail5Flow);
-
-#[derive(Flow)]
-pub struct LocalTail3Flow(Step<LocalTailStub3Spec>, LocalTail4Flow);
-
-#[derive(Flow)]
-pub struct LocalTail2Flow(Step<LocalTailStub2Spec>, LocalTail3Flow);
-
-#[derive(Flow)]
-pub struct LocalTail1Flow(Step<LocalTailStub1Spec>, LocalTail2Flow);
-
-#[derive(Flow)]
-pub struct LocalPostConditionalFlow(Step<FlattenConditionalEitherSpec>, LocalTail1Flow);
-
-#[derive(Flow)]
 pub struct LocalConditionalJoinTailFlow(
     Conditional<LocalConditionalPrefersLeft, LocalConditionalLeftBranch, LocalConditionalRightBranch>,
-    LocalPostConditionalFlow,
+    Step<LocalTailStubSpec>,
+    Step<LocalTailStubSpec>,
+    Step<LocalTailStubSpec>,
+    Step<LocalTailStubSpec>,
+    Step<LocalTailStubSpec>,
+    Step<LocalTailStubSpec>,
+    Step<LocalTailStubSpec>,
+    Step<LocalTailStubSpec>,
+    Step<LocalTailStubSpec>,
+    Step<LocalTailStubSpec>,
 );
 
 pub struct LocalConditionalJoinTailAnimal;
