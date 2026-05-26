f6dd4f5102fb6aca7a07539514d772105523c33d
The Man in the Box <mitbagent@gmail.com>
2026-05-26 02:00:35 -0700

    chore: Address comment 5993

--- jungle-examples/examples/welcome/instrumentation/vocals/mod.rs
+++ jungle-examples/examples/welcome/instrumentation/vocals/mod.rs
@@ -43,27 +43,6 @@ impl Instrument for Vocals {
     }
 }
 
-pub struct Sing<const NOTE: u8, const NOTE_TICK: u32, const REST_TICK: u32, const LANE_ID: u32 = 0>;
-#[jungle::act]
-impl<const NOTE: u8, const NOTE_TICK: u32, const REST_TICK: u32, const LANE_ID: u32> Act
-    for Sing<NOTE, NOTE_TICK, REST_TICK, LANE_ID>
-{
-    type Effect = Monad<Vocals, VocalsArticulation, LANE_ID, NOTE, NOTE_TICK, REST_TICK>;
-    type Input = ();
-    type Output = ();
-
-    fn emit(state: &VocalsArticulation, _input: Self::Input) -> <Self::Effect as EffectSchema>::In {
-        *state
-    }
-
-    fn absorb(
-        _state: &mut VocalsArticulation,
-        output: EffectCompletion<Self::Effect>,
-    ) -> Self::Output {
-        output.expect("note playback should succeed");
-    }
-}
-
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct Lyrics {
     pub phonemes: Vec<[Option<Phoneme>; 12]>,
