326d3bffd3b6467f1cabd64020cbc801f193612f
The Man in the Box <mitbagent@gmail.com>
2026-05-28 01:20:47 -0700

    Revert "chore: Address comment 6584"
    This reverts commit 166a07d4b8c72c46d7cc1a0a7e9457b601d5b9cd.

--- jungle-types/src/meta/mod.rs
+++ jungle-types/src/meta/mod.rs
@@ -1,7 +1,6 @@
 use typosaurus::bool::{And, Not, Truthy};
 use typosaurus::cmp::Equality;
 use typosaurus::cmp::IsEqual;
-use typosaurus::collections::list::Interleave as ListInterleave;
 use typosaurus::collections::{
     list,
     sp::{FlattenNodes, Node, SPFlatten},
@@ -710,35 +709,16 @@ where
     <BoundAnimalJourney<H7> as JourneyEffects>::List: FlattenNodes,
     SPFlatten<<BoundAnimalJourney<H7> as JourneyEffects>::List>: KeepEffectNodes,
     Tail: CollectAnimalJourneyEffects,
-    (
-        list_chain!(
-            <SPFlatten<<BoundAnimalJourney<H0> as JourneyEffects>::List> as KeepEffectNodes>::Out,
-            <SPFlatten<<BoundAnimalJourney<H1> as JourneyEffects>::List> as KeepEffectNodes>::Out,
-            <SPFlatten<<BoundAnimalJourney<H2> as JourneyEffects>::List> as KeepEffectNodes>::Out,
-            <SPFlatten<<BoundAnimalJourney<H3> as JourneyEffects>::List> as KeepEffectNodes>::Out
-        ),
-        list_chain_tail!(
-            <SPFlatten<<BoundAnimalJourney<H4> as JourneyEffects>::List> as KeepEffectNodes>::Out,
-            <SPFlatten<<BoundAnimalJourney<H5> as JourneyEffects>::List> as KeepEffectNodes>::Out,
-            <SPFlatten<<BoundAnimalJourney<H6> as JourneyEffects>::List> as KeepEffectNodes>::Out,
-            <SPFlatten<<BoundAnimalJourney<H7> as JourneyEffects>::List> as KeepEffectNodes>::Out ;
-            <Tail as CollectAnimalJourneyEffects>::Out
-        ),
-    ): ListInterleave,
 {
-    type Out = <(
-        list_chain!(
-            <SPFlatten<<BoundAnimalJourney<H0> as JourneyEffects>::List> as KeepEffectNodes>::Out,
-            <SPFlatten<<BoundAnimalJourney<H1> as JourneyEffects>::List> as KeepEffectNodes>::Out,
-            <SPFlatten<<BoundAnimalJourney<H2> as JourneyEffects>::List> as KeepEffectNodes>::Out,
-            <SPFlatten<<BoundAnimalJourney<H3> as JourneyEffects>::List> as KeepEffectNodes>::Out
-        ),
-        list_chain_tail!(
-            <SPFlatten<<BoundAnimalJourney<H4> as JourneyEffects>::List> as KeepEffectNodes>::Out,
-            <SPFlatten<<BoundAnimalJourney<H5> as JourneyEffects>::List> as KeepEffectNodes>::Out,
-            <SPFlatten<<BoundAnimalJourney<H6> as JourneyEffects>::List> as KeepEffectNodes>::Out,
-            <SPFlatten<<BoundAnimalJourney<H7> as JourneyEffects>::List> as KeepEffectNodes>::Out ;
-            <Tail as CollectAnimalJourneyEffects>::Out
-        ),
-    ) as ListInterleave>::Out;
+    type Out = list_chain_tail!(
+        <SPFlatten<<BoundAnimalJourney<H0> as JourneyEffects>::List> as KeepEffectNodes>::Out,
+        <SPFlatten<<BoundAnimalJourney<H1> as JourneyEffects>::List> as KeepEffectNodes>::Out,
+        <SPFlatten<<BoundAnimalJourney<H2> as JourneyEffects>::List> as KeepEffectNodes>::Out,
+        <SPFlatten<<BoundAnimalJourney<H3> as JourneyEffects>::List> as KeepEffectNodes>::Out,
+        <SPFlatten<<BoundAnimalJourney<H4> as JourneyEffects>::List> as KeepEffectNodes>::Out,
+        <SPFlatten<<BoundAnimalJourney<H5> as JourneyEffects>::List> as KeepEffectNodes>::Out,
+        <SPFlatten<<BoundAnimalJourney<H6> as JourneyEffects>::List> as KeepEffectNodes>::Out,
+        <SPFlatten<<BoundAnimalJourney<H7> as JourneyEffects>::List> as KeepEffectNodes>::Out ;
+        <Tail as CollectAnimalJourneyEffects>::Out
+    );
 }
