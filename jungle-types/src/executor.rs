77bd3576d9c9e4551c283d53f19b4bd29471a8ef
The Man in the Box <mitbagent@gmail.com>
2026-05-31 15:51:39 -0700

    chore: Address comment 7302

--- jungle-types/src/executor.rs
+++ jungle-types/src/executor.rs
@@ -500,34 +500,11 @@ where
         }
         if core::any::type_name::<<A as BoundAction<T>>::Effect>() == core::any::type_name::<Noop>()
         {
-            let typed_input = match deserialize_step_input::<A::Input>(&input) {
-                Ok(typed_input) => typed_input,
-                Err(err) => return Err((state, err)),
-            };
-            let (state, (request, carry)) =
-                <BoundFlowStep<T, A> as Running>::run((state, typed_input));
-            self.pending_carry = Some(carry);
-            let request = match postcard::to_allocvec(&request.into_input()) {
-                Ok(request) => request,
-                Err(err) => return Err((state, ExecutorError::RequestSerialize(err.to_string()))),
-            };
-            let runner: EffectRunner = Box::new(move || {
-                Box::pin(async move {
-                    let completion = Ok(postcard::to_allocvec(&())
-                        .map_err(|err| ExecutorError::OutputSerialize(err.to_string()))?);
-                    Ok(completion)
-                })
-            });
-            self.waiting_completion = true;
-            return Ok((
-                state,
-                ExecutableEffectRequest::new(
-                    self.node_id,
-                    core::any::type_name::<<A as BoundAction<T>>::Effect>(),
-                    request,
-                    runner,
-                ),
-            ));
+            if let Err(err) = deserialize_step_input::<A::Input>(&input) {
+                return Err((state, err));
+            }
+            self.pending_inline_input = Some(input);
+            return Err((state, ExecutorError::Complete));
         }
 
         let typed_input = match deserialize_step_input::<A::Input>(&input) {
@@ -729,34 +706,11 @@ where
         }
         if core::any::type_name::<<A as BoundAction<T>>::Effect>() == core::any::type_name::<Noop>()
         {
-            let typed_input = match deserialize_step_input::<A::Input>(&input) {
-                Ok(typed_input) => typed_input,
-                Err(err) => return Err((state, err)),
-            };
-            let (state, (request, carry)) =
-                <BoundFlowStep<T, A> as Running>::run((state, typed_input));
-            self.pending_carry = Some(carry);
-            let request = match postcard::to_allocvec(&request.into_input()) {
-                Ok(request) => request,
-                Err(err) => return Err((state, ExecutorError::RequestSerialize(err.to_string()))),
-            };
-            let runner: EffectRunner = Box::new(move || {
-                Box::pin(async move {
-                    let completion = Ok(postcard::to_allocvec(&())
-                        .map_err(|err| ExecutorError::OutputSerialize(err.to_string()))?);
-                    Ok(completion)
-                })
-            });
-            self.waiting_completion = true;
-            return Ok((
-                state,
-                ExecutableEffectRequest::new(
-                    self.node_id,
-                    core::any::type_name::<<A as BoundAction<T>>::Effect>(),
-                    request,
-                    runner,
-                ),
-            ));
+            if let Err(err) = deserialize_step_input::<A::Input>(&input) {
+                return Err((state, err));
+            }
+            self.pending_inline_input = Some(input);
+            return Err((state, ExecutorError::Complete));
         }
 
         let typed_input = match deserialize_step_input::<A::Input>(&input) {
