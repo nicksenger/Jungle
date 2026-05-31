0cb7001185fdcdf61b21053d61a22844e765c668
The Man in the Box <mitbagent@gmail.com>
2026-05-31 13:30:51 -0700

    chore: Address comment 7259

--- jungle-examples/examples/cronfish/mod.rs
+++ jungle-examples/examples/cronfish/mod.rs
@@ -1,9 +1,6 @@
 use jungle_sdk::prelude::*;
 use std::str::FromStr;
 
-#[derive(Default)]
-pub struct CronfishState(pub Option<cron::Schedule>);
-
 pub struct CronfishTick;
 #[jungle::action]
 impl Action for CronfishTick {
@@ -11,9 +8,9 @@ impl Action for CronfishTick {
     type Input = ();
     type Output = ();
 
-    fn emit(_state: &CronfishState, _input: Self::Input) -> () {}
+    fn emit(_state: &(), _input: Self::Input) -> () {}
 
-    fn absorb(_state: &mut CronfishState, output: EffectCompletion<Self::Effect>) -> Self::Output {
+    fn absorb(_state: &mut (), output: EffectCompletion<Self::Effect>) -> Self::Output {
         output.expect("cronfish noop step should complete");
     }
 }
@@ -24,17 +21,14 @@ pub struct CronfishJourney(Step<CronfishTick>);
 pub struct Cronfish;
 #[jungle::animal(id = 0, generation = 0)]
 impl Animal for Cronfish {
-    type State = CronfishState;
+    type State = ();
     type Seed = ();
     type Journey = CronfishJourney;
 }
 
 fn main() {
-    let mut state = CronfishState::default();
-    state.0 = Some(
-        cron::Schedule::from_str("0 */5 * * * * *")
-            .expect("example cronfish schedule should parse"),
-    );
-    let _executor = Executor::<Cronfish>::new(state);
+    let _schedule = cron::Schedule::from_str("0 */5 * * * * *")
+        .expect("example cronfish schedule should parse");
+    let _executor = Executor::<Cronfish>::new(());
     println!("cronfish animal initialized");
 }
