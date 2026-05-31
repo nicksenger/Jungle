aac6e1180c68062094776e6a5424925d4b8d69fc
The Man in the Box <mitbagent@gmail.com>
2026-05-31 16:15:45 -0700

    chore: Address comment 7302

--- jungle-types/src/error.rs
+++ jungle-types/src/error.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::error::Error as StdError;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
@@ -10,4 +11,25 @@ pub enum Error {
 pub enum Failure {
     #[error("{0}")]
     Message(String),
+}
+
+impl From<String> for Failure {
+    fn from(value: String) -> Self {
+        Self::Message(value)
+    }
+}
+
+impl From<&str> for Failure {
+    fn from(value: &str) -> Self {
+        Self::Message(value.to_owned())
+    }
+}
+
+impl<E> From<Box<E>> for Failure
+where
+    E: StdError + Send + Sync + 'static + ?Sized,
+{
+    fn from(value: Box<E>) -> Self {
+        Self::Message(value.to_string())
+    }
 }
